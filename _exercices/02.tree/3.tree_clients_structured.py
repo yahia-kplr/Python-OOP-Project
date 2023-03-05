e22e51c233f93a20a33a26ab91410fe865b379f3
mehdi-lamrani <mehdi.lamrani@kplr.fr>
2023-03-05 13:52:36 +0100

    .

--- _exercices/02.tree/3.tree_clients_structured.py
+++ _exercices/02.tree/3.tree_clients_structured.py
@@ -28,6 +28,13 @@ from unidecode import unidecode
 from treelib import Tree
 
 def json_dict_from_file():
+    """
+    Cette fonction ouvre et charge les données JSON du fichier
+    dans un dictionnaire Python.
+
+    Returns:
+        dict: le dictionnaire Python contenant les données JSON du fichier
+    """
     # Chargement des données JSON à partir du fichier dans un dictionnaire python
     json_data = json.load(open("_exercices/01.json/json_data.json", "rb"))
 
@@ -44,6 +51,15 @@ def json_dict_from_file():
     return json_dict
 
 def create_tree_from_dict(tree, parent_node_id, parent_dict):
+    """
+    Cette fonction crée un arbre à partir d'un dictionnaire.
+    Elle est appelée récursivement pour chaque sous-dictionnaire.
+
+    Args:
+        tree (Tree): un objet Tree de la bibliothèque treelib pour représenter l'arbre
+        parent_node_id (str): l'identifiant du noeud parent dans l'arbre
+        parent_dict (dict): le dictionnaire Python contenant les données à insérer dans l'arbre
+    """
     for key, value in parent_dict.items():
         if isinstance(value, dict):
             # Créer un nouveau noeud pour la clé courante du dictionnaire
@@ -58,19 +74,24 @@ def create_tree_from_dict(tree, parent_node_id, parent_dict):
             tree.create_node(tag=f"{key}: {value}", identifier=leaf_node_id, parent=parent_node_id)
 
 def main():
+    """
+    Cette fonction est la fonction principale qui orchestre toutes les autres.
+    Elle charge les données JSON depuis un fichier, crée un objet Tree de la bibliothèque treelib,
+    et crée un arbre à partir des données JSON.
 
+    Elle affiche ensuite l'arbre créé.
+    """
     my_tree = Tree()
     # Créer le noeud racine pour l'arbre
     my_tree.create_node(tag="Racine", identifier="racine")
 
-    json_dict=json_dict_from_file()
-
-    # Créer la structure de l'arbre à partir du dictionnaire
+    # Charger les données JSON depuis un fichier et créer la structure de l'arbre à partir du dictionnaire
+    json_dict = json_dict_from_file()
     create_tree_from_dict(my_tree, "racine", json_dict)
 
     # Afficher l'arbre
     my_tree.show()
 
 if __name__ == '__main__':
-    
+    # Appeler la fonction principale
     main()
